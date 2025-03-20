cf3d8c1b45168043cb83691a6009e0efcf8ffdf3
Erika Sundman <erika.sundman@bjornlunden.com>
2025-03-20 21:04:18 +0000

    cleaned the code

--- src/test/java/stepDefs/BasketballEnglandStepDefs.java
+++ src/test/java/stepDefs/BasketballEnglandStepDefs.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-
 import java.time.Duration;
 
 public class BasketballEnglandStepDefs {
@@ -22,7 +20,6 @@ public class BasketballEnglandStepDefs {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
         element.click();
     }
-
 
     @After
     public void teardown() {
@@ -36,18 +33,11 @@ public class BasketballEnglandStepDefs {
         driver = new ChromeDriver();
         driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
         // Verify page is loaded
-//        new WebDriverWait(driver, Duration.ofSeconds(10))
-//                // .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#dp")));
-//                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@id='signup_form']")));
         waitAndClick(By.xpath("(//form[@id='signup_form'])"));
     }
 
     @When("Jag fyller i registreringsformularet med giltig information")
     public void jag_fyller_i_registreringsformularet_med_giltig_information() {
-
-//    @When("Jag fyller i registreringsformuläret med giltig information")
-//    public void jagFyllerIRegistreringsformularetMedGiltigInformation() {
-
 
         // Fyller i namn
         driver.findElement(By.cssSelector("#member_firstname")).sendKeys("Isak");
@@ -62,18 +52,12 @@ public class BasketballEnglandStepDefs {
         driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys("AirJordan23!");
 
         // Checkboxes for terms
-
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        WebElement agreeToTerms = wait.until(ExpectedConditions.elementToBeClickable(By.id("agreeToTerms")));
-//        agreeToTerms.click();
-
         driver.findElement(By.cssSelector("label[for=sign_up_25] > span.box")).click();
         driver.findElement(By.cssSelector("label[for=sign_up_26] > span.box")).click();
         driver.findElement(By.cssSelector("label[for=fanmembersignup_agreetocodeofethicsandconduct] > span.box")).click();
 
         // Ange Date of birth
         driver.findElement(By.cssSelector("#dp")).sendKeys("26/12/1999");
-
     }
 
     @When("Jag klickar pa Confirm and Join knappen")
@@ -88,17 +72,9 @@ public class BasketballEnglandStepDefs {
         WebElement successMessage = new WebDriverWait(driver, Duration.ofSeconds(50))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.background-gray > h2.bold.gray.text-center.margin-bottom-40")));
 
-
         driver.findElement(By.cssSelector("div.background-gray > h2.bold.gray.text-center.margin-bottom-40"));
 
         // Verify success message is displayed
-        //THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND
-
-
-        //  assert successMessage.isDisplayed() : "Success message was not displayed";
-
-
+        assert successMessage.isDisplayed() : "Success message was not displayed";
     }
-
-
 }
